048b3b5ca09df85c3cc99f8817b229e71f2c573c
Rostom Sahakian <rostom.sahakian@gmail.com>
2021-09-11 21:57:36 -0700

    RS worked on show users sort table heads

--- resources/views/admin/Modules/Site_Settings/UserManagement/partials/showusers.blade.php
+++ resources/views/admin/Modules/Site_Settings/UserManagement/partials/showusers.blade.php
@@ -1,40 +1,44 @@
 @if (is_countable($users) && count($users) > 0)
     <form action="">
         <table class="table table-bordered table-striped table-hover">
-            <thead>
+            <thead id="users_table_data">
                 <tr>
                     {{-- ('bi bi-sort-numeric-up-alt', 'bi bi-sort-numeric-down'); --}}
                     {{-- sortListsBy(selector, by, direction, icon_name, switch_icon) --}}
-                    <th>
-                        <a class="text-muted" id="sort-by_id_asc"
-                            onclick="sortListsBy('sort-by_id_asc', 'id', '{{ $direction }}', 'bi bi-sort-numeric-down', 'bi bi-sort-numeric-up-alt' );">Id&nbsp;
+                    <th
+                        onclick="sortListsBy('sort-by_id_asc', 'id', '{{ $direction }}', 'bi bi-sort-numeric-down', 'bi bi-sort-numeric-up-alt' );">
+                        <a class="text-muted" id="sort-by_id_asc">Id&nbsp;
                             <i class="bi bi-sort-numeric-{{ $icon_directions->id_icon }} h5">
                             </i>
                         </a>
                     </th>
-                    <th><a class="text-muted" id="sort-by_name_asc"
-                            onclick="sortListsBy('sort-by_name_asc', 'name', '{{ $direction }}', 'bi bi-sort-alpha-down', 'bi bi-sort-alpha-up-alt' );">Name&nbsp;
+                    <th
+                        onclick="sortListsBy('sort-by_name_asc', 'name', '{{ $direction }}', 'bi bi-sort-alpha-down', 'bi bi-sort-alpha-up-alt' );">
+                        <a class="text-muted" id="sort-by_name_asc">Name&nbsp;
                             <i class="bi bi-sort-alpha-{{ $icon_directions->name_icon }} h5">
                             </i>
                         </a>
                     </th>
-                    <th><a class="text-muted" id="sort-by_email_asc"
-                            onclick="sortListsBy('sort-by_email_asc', 'email', '{{ $direction }}', 'bi bi-sort-alpha-down', 'bi bi-sort-alpha-up-alt' );">Email&nbsp;<i
-                                class="bi bi-sort-alpha-{{ $icon_directions->email_icon }} h5"></i>
+                    <th
+                        onclick="sortListsBy('sort-by_email_asc', 'email', '{{ $direction }}', 'bi bi-sort-alpha-down', 'bi bi-sort-alpha-up-alt' );">
+                        <a class="text-muted" id="sort-by_email_asc">Email&nbsp;
+                            <i class="bi bi-sort-alpha-{{ $icon_directions->email_icon }} h5"></i>
                         </a>
                     </th>
 
 
-                    <th><a class="text-muted" id="sort-by_role_asc"
-                            onclick="sortListsBy('sort-by_role_asc', 'roles_id', '{{ $direction }}', 'bi bi-sort-alpha-down', 'bi bi-sort-alpha-up-alt' );">Role
-                            Id&nbsp;<i class="bi bi-sort-alpha-{{ $icon_directions->role_icon }} h5"></i>
+                    <th
+                        onclick="sortListsBy('sort-by_role_asc', 'roles_id', '{{ $direction }}', 'bi bi-sort-alpha-down', 'bi bi-sort-alpha-up-alt' );">
+                        <a class="text-muted" id="sort-by_role_asc">RoleId&nbsp;
+                            <i class="bi bi-sort-alpha-{{ $icon_directions->role_icon }} h5"></i>
                         </a>
                     </th>
-                    <th><a class="text-muted" id="sort-by_cd_asc"
-                        onclick="sortListsBy('sort-by_cd_asc', 'created_at', '{{ $direction }}', 'bi bi-sort-numeric-down', 'bi bi-sort-numeric-up-alt' );">Created At&nbsp;<i
-                                class="bi bi-sort-numeric-{{ $icon_directions->cd_icon }} h5"></i>
-                            </a>
-                        </th>
+                    <th
+                        onclick="sortListsBy('sort-by_cd_asc', 'created_at', '{{ $direction }}', 'bi bi-sort-numeric-down', 'bi bi-sort-numeric-up-alt' );">
+                        <a class="text-muted" id="sort-by_cd_asc">CreatedAt&nbsp;
+                            <i class="bi bi-sort-numeric-{{ $icon_directions->cd_icon }} h5"></i>
+                        </a>
+                    </th>
                 </tr>
                 <input type="hidden" id="current_page_num" value="{{ $users->currentPage() }}" />
                 <input type="hidden" id="current_sort_direction" value="{{ $current_direction }}" />
